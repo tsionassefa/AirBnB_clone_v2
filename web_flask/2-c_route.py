5d8b9782b3d6bf635c6594f40aee401d2b947fb5
tsionassefa <tsionassefa101@gmail.com>
2023-05-19 14:26:57 +0300

    ch

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""flask class"""
+from flask import Flask
+"""Flask class"""
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def helo_hbnb():
+    """displays text
+    Returns:
+        text
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def disp_hbnb():
+    """displays text
+    Returns:
+        text
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def disp_text(text):
+    """displays text
+    Args:
+        text (str): text
+    Returns:
+        text
+    """
+    return 'C %s' % text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
