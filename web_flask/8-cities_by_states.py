02f131ebb92dfdc5053f34d72047b3815e8fcf43
tsionassefa <tsionassefa101@gmail.com>
2023-05-19 15:49:18 +0300

    comment

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+""" flask import"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.city import City
+"""import flask"""
 
 
 app = Flask(__name__)
