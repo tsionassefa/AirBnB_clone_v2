d9b5b1230a79493a4e300fb663bad6e15ac88fc5
tsionassefa <tsionassefa101@gmail.com>
2023-05-19 12:44:45 +0300

    done

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,12 +8,13 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hel_hbnb():
-    """display message
-    return the text
+def helo_hbnb():
+    """displays text
+    Returns:
+        text
     """
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
-    
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
