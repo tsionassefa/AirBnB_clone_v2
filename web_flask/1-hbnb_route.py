9e80ee403059b6e869bdcff8cdeb94e90ed3e15b
tsionassefa <tsionassefa101@gmail.com>
2023-05-19 14:13:25 +0300

    done

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+from flask import Flask
+"""Flask class"""
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def helo_hbnb():
+    """displays message
+    Returns:
+        message
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def disp_hbnb():
+    """displays text
+    Returns:
+        text
+    """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
